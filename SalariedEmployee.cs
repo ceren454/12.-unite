a52fd936516bbc0295140bcf1632117c12ccef6c
Ceren Toy <cerebtoy@gmail.com>
2025-12-28 14:18:15 +0300

    Create SalariedEmployee.cs

---
+++ SalariedEmployee.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class SalariedEmployee : Employee
+{
+    private decimal weeklySalary;
+
+    public SalariedEmployee(
+        string firstName,
+        string lastName,
+        string socialSecurityNumber,
+        decimal weeklySalary)
+        : base(firstName, lastName, socialSecurityNumber)
+    {
+        WeeklySalary = weeklySalary;
+    }
+
+    public decimal WeeklySalary
+    {
+        get => weeklySalary;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(value),
+                    value,
+                    $"{nameof(WeeklySalary)} must be >= 0");
+            }
+
+            weeklySalary = value;
+        }
+    }
+
+    public override decimal Earnings() => WeeklySalary;
+
+    public override string ToString() =>
+        $"salaried employee: {base.ToString()}\n" +
+        $"weekly salary: {WeeklySalary:C}";
+}
